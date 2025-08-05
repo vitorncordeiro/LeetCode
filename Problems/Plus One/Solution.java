64944325fb492ab3fb0821d7f1ac85a7b6ec81d3
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-05 17:54:28 -0300

    Update Solution.java

--- Problems/Plus One/Solution.java
+++ Problems/Plus One/Solution.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int[] plusOne(int[] digits) {
+        String str = "";
+        for(int i=0; i < digits.length; i++){
+            str += digits[i];
+        }
+        Integer num = Integer.parseInt(str);
+        num ++;
+        int[] finalArray = new int[num.toString().length()];
+        for(int i= finalArray.length - 1; i >= 0; i--){
+            int numRest = num % 10;
+            finalArray[i] = numRest;
+            num -= numRest;
+            num /= 10;
+        }
+        return finalArray;
+    }
+}
