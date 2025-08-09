f00c55355d6abd537cc236545e34eb725278c52b
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-09 11:33:26 -0300

    Update Solution.java

--- Problems/Find Words Containing Character/Solution.java
+++ Problems/Find Words Containing Character/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
         ArrayList<Integer> ans = new ArrayList<>();
         for(int i =0; i < words.length; i++){
-            if(words[i].contains(String.valueOf(x))){
+            if(words[i].indexOf(x) != -1){
                 ans.add(i);
             }
         }
